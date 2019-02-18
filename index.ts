9bdf55fe1c0892f1a399099506a37968cb552a3d
Andrei Fidelman <fidelmanlife@gmail.com>
2019-02-18 22:18:47 +0100

    Use Types vs. Interfaces

--- index.ts
+++ index.ts
@@ -1,23 +1,76 @@
-interface IPet {
-  name: string
+// type Pet = IDog | ICat
+
+interface IAnimal {
   age: number
-  park?: string
+  eat(): void
+  speak(): string
 }
 
-type ReadonlyPet = {
-  +readonly [K in keyof IPet]-?: IPet[K] // remove maybe props
+type AnimalTypeAlias = {
+  age: number,
+  eat(): void,
+  speak(): string,
 }
 
-const pet: IPet = {
-  name: 'Buttons',
-  age: 5,
+function feedAnimal(animal: IAnimal) {
+  animal.eat()
 }
 
-const readonlyPet: ReadonlyPet = {
-  name: 'Buttons',
-  age: 5,
-  park: 'Rigrac',
+class Animal implements IAnimal {
+  age = 0
+
+  eat() {
+    console.log('nom..nom..')
+  }
+
+  speak() {
+    return 'woof'
+  }
 }
 
-pet.age = 6
-readonlyPet.age = 6
+///// Similar ////
+type Eat = (food: string) => void
+type AnimalList = string[]
+
+interface IEat {
+  (food: string): void
+}
+
+interface IAnimalList {
+  [index: number]: string
+}
+
+interface IPet {
+  pose(): void
+}
+
+interface IFeline {
+  nightvision: boolean
+}
+
+type Cat = IPet & IFeline
+interface ICat extends IPet, IFeline {
+}
+
+///// Diff //////
+type PetType = IDog | ICat
+interface IDog {}
+interface ICat {}
+interface IPet extends PetType {}
+class Pet implements PetType {}
+
+
+interface IFoo {
+  a: string
+}
+interface IFoo {
+  b: string
+}
+let foo: IFoo
+
+type Foo = {
+  a: string
+}
+type Foo = {
+  b: string
+}
